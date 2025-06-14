2bc94a7bf51d2308c6b70f3e0d1f356854bffb75
PapaHamf <jaroslaw.puchala@gmail.com>
2025-06-14 15:10:29 +0200

    fixed allure

--- pageobjects/home_page.py
+++ pageobjects/home_page.py
@@ -5,6 +5,7 @@ from selenium.webdriver.remote.webelement import WebElement
 class HomePage:
 
     MY_ACCOUNT: tuple = (By.CSS_SELECTOR, "li[id='menu-item-22'] a[class='nav__link']")
+    # //ul[@id='topbar-menu']//a[./span[text()='My account']]
 
     def __init__(self, driver):
         self._driver = driver
